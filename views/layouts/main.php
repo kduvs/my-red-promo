6260cab1348eaf5b528d980a8e41fede91ea95ed
kduvs <vernitemnemoi2007@bk.ru>
2023-02-09 14:58:52 +0800

    style: Изменен цвет навбара

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -34,7 +34,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
+        'options' => ['class' => 'navbar-expand-md navbar-dark bg-primary fixed-top']
     ]);
     $menuItems = [
         ['label' => 'Главная', 'url' => ['/site/index']],
